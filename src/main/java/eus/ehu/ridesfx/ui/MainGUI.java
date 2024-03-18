738e6049600a3c1ff6c46cd2fb46d95af8b004af
AdeiTamayo <atamayo012@ikasle.ehu.eus>
2024-03-18 13:51:56 +0100

    Initial commit

--- src/main/java/eus/ehu/ridesfx/ui/MainGUI.java
+++ src/main/java/eus/ehu/ridesfx/ui/MainGUI.java
@@ -1,6 +1,7 @@
 package eus.ehu.ridesfx.ui;
 
 import eus.ehu.ridesfx.businessLogic.BlFacade;
+import eus.ehu.ridesfx.uicontrollers.MainGUIController;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.kordamp.bootstrapfx.BootstrapFX;
 import eus.ehu.ridesfx.uicontrollers.Controller;
+import eus.ehu.ridesfx.uicontrollers.MainGUIController;
 
 import java.io.IOException;
 import java.util.Locale;
@@ -15,11 +17,10 @@ import java.util.ResourceBundle;
 
 public class MainGUI {
 
-    private Window mainWin, createRideWin, queryRidesWin;
-
     private BlFacade businessLogic;
     private Stage stage;
     private Scene scene;
+    private MainGUIController mainGUIController; // Declare MainGUIController instance
 
     public BlFacade getBusinessLogic() {
         return businessLogic;
@@ -33,6 +34,7 @@ public class MainGUI {
         Platform.startup(() -> {
             try {
                 setBusinessLogic(bl);
+                mainGUIController = new MainGUIController(bl); // Initialize MainGUIController instance
                 init(new Stage());
             } catch (IOException e) {
                 e.printStackTrace();
@@ -40,73 +42,19 @@ public class MainGUI {
         });
     }
 
-
-    class Window {
-        Controller c;
-        Parent ui;
-    }
-
-    private Window load(String fxmlfile) throws IOException {
-        Window window = new Window();
-        FXMLLoader loader = new FXMLLoader(MainGUI.class.getResource(fxmlfile), ResourceBundle.getBundle("Etiquetas", Locale.getDefault()));
-        loader.setControllerFactory(controllerClass -> {
-            try {
-                return controllerClass
-                        .getConstructor(BlFacade.class)
-                        .newInstance(businessLogic);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        });
-        window.ui = loader.load();
-        ((Controller) loader.getController()).setMainApp(this);
-        window.c = loader.getController();
-        return window;
-    }
-
     public void init(Stage stage) throws IOException {
-
-        this.stage = stage;
-
-        mainWin = load("MainGUI.fxml");
-        queryRidesWin = load("QueryRides.fxml");
-        createRideWin = load("CreateRide.fxml");
-
         showMain();
-
     }
-
-//  public void start(Stage stage) throws IOException {
-//      init(stage);
-//  }
-
 
     public void showMain() {
-        setupScene(mainWin.ui, "MainTitle", 320, 250);
+        mainGUIController.showScene("main");
     }
 
     public void showQueryRides() {
-        setupScene(queryRidesWin.ui, "QueryRides", 1000, 500);
+        mainGUIController.showScene("queryRides");
     }
 
     public void showCreateRide() {
-        setupScene(createRideWin.ui, "CreateRide", 550, 400);
+        mainGUIController.showScene("createRide");
     }
-
-    private void setupScene(Parent ui, String title, int width, int height) {
-        if (scene == null) {
-            scene = new Scene(ui, width, height);
-            scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
-            stage.setScene(scene);
-        }
-        stage.setWidth(width);
-        stage.setHeight(height);
-        stage.setTitle(ResourceBundle.getBundle("Etiquetas", Locale.getDefault()).getString(title));
-        scene.setRoot(ui);
-        stage.show();
-    }
-
-//  public static void main(String[] args) {
-//    launch();
-//  }
 }
--- src/main/java/eus/ehu/ridesfx/uicontrollers/MainGUIController.java
+++ src/main/java/eus/ehu/ridesfx/uicontrollers/MainGUIController.java
@@ -3,14 +3,19 @@ package eus.ehu.ridesfx.uicontrollers;
 import eus.ehu.ridesfx.businessLogic.BlFacade;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import eus.ehu.ridesfx.ui.MainGUI;
+import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MainGUIController implements Controller{
+public class MainGUIController {
+    private Window CreateRideWin, QueryRidesWin, MainWin;
 
     @FXML
     private Label selectOptionLbl;
@@ -35,33 +40,84 @@ public class MainGUIController implements Controller{
     private MainGUI mainGUI;
 
     private BlFacade businessLogic;
+    /*
+    public MainGUIController() {}
+    */
 
-    public MainGUIController(){};
-
-    public MainGUIController(BlFacade blFacade){
+    //Borderpane kodean ez dago baina programa honen funtzionamendurako behar da
+    public MainGUIController(BlFacade blFacade) {
         businessLogic = blFacade;
     }
 
+
+
+
+    public class Window {
+        private Controller controller;
+        private Parent ui;
+    }
+
+    private Window load(String fxml) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+            Parent ui = loader.load();
+            Controller controller = loader.getController();
+
+            Window window = new Window();
+            window.controller = controller;
+            window.ui = ui;
+            return window;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @FXML
+    private BorderPane mainPane;
+
     @FXML
     void queryRides(ActionEvent event) {
-        mainGUI.showQueryRides();
+        showScene("queryRides");
     }
 
     @FXML
     void createRide(ActionEvent event) {
-        mainGUI.showCreateRide();
+        showScene("createRide");
     }
+
+
+
+    /*
+    public void setMainApp(MainGUI mainGUI) {
+        this.mainGUI = mainGUI;
+    }
+
+     */
+
 
 
     @FXML
     void initialize() {
 
-            // set current driver name
-            lblDriver.setText(businessLogic.getCurrentDriver().getName());
+        CreateRideWin = load("CreateRide.fxml");
+        QueryRidesWin = load("QueryRides.fxml");
+        MainWin = load("MainGUI.fxml");
+
+        showScene("main");
+
+        // set current driver name
+        lblDriver.setText(businessLogic.getCurrentDriver().getName());
     }
 
-    @Override
-    public void setMainApp(MainGUI mainGUI) {
-        this.mainGUI = mainGUI;
+    public void showScene(String scene) {
+        switch (scene) {
+            case "createRide" -> mainPane.setCenter(CreateRideWin.ui);
+            case "queryRides" -> mainPane.setCenter(QueryRidesWin.ui);
+            case "main" -> mainPane.setCenter(MainWin.ui);
+
+        }
     }
 }
+
+//FIXME error: java.lang.NullPointerException: Cannot read field "ui" because "this.MainWin" is null
