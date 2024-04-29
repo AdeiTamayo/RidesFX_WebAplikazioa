276ed4ffdb9e382cc02a6d88e314c5ed2bf49a29
AdeiTamayo <atamayo012@ikasle.ehu.eus>
2024-04-29 22:39:44 +0200

    Fixed domain

--- src/main/java/eus/ehu/ridesfx/domain/CityData.java
+++ src/main/java/eus/ehu/ridesfx/domain/CityData.java
@@ -1,31 +1,58 @@
 package eus.ehu.ridesfx.domain;
 
 public class CityData {
-    private String name;
-    private String country;
+    private String toponymName;
+    private String countryName;
     private String citizenCount;
-    private String imageUrl;
+    private int population;
 
-    public CityData(String name, String country, String citizenCount, String imageUrl) {
-        this.name = name;
-        this.country = country;
+    public CityData(String toponymName, String countryName, String citizenCount, int population) {
+        this.toponymName = toponymName;
+        this.countryName = countryName;
         this.citizenCount = citizenCount;
-        this.imageUrl = imageUrl;
+        this.population = population;
     }
 
-    public String getName() {
-        return name;
+    public String getToponymName() {
+        return toponymName;
     }
 
-    public String getCountry() {
-        return country;
+    public void setToponymName(String toponymName) {
+        this.toponymName = toponymName;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
     }
 
     public String getCitizenCount() {
         return citizenCount;
     }
 
-    public String getImageUrl() {
-        return imageUrl;
+    public void setCitizenCount(String citizenCount) {
+        this.citizenCount = citizenCount;
     }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return "CityData{" +
+                "toponymName='" + toponymName + '\'' +
+                ", countryName='" + countryName + '\'' +
+                ", citizenCount='" + citizenCount + '\'' +
+                ", population=" + population +
+                '}';
+    }
+
 }
